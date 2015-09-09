d1139742c52cbea479254da09355401dbc7cc093
Mark <mzroth@gmail.com>
2015-09-09 12:11:10 -0600

    added map1 to Maps.js and getMap function

---
+++ backend/Maps.js
@@ -0,0 +1,18 @@
+function getMap(mapName) {
+	return maps(mapName);
+}
+
+maps {
+	// w = water l = land
+	map1 =[[w,w,w,w,l,l,l,l,l,l],
+	[w,w,w,l,l,l,l,l,l,l],
+	[w,w,l,l,l,l,l,l,l,l],
+	[w,l,l,l,l,l,l,l,l,l],
+	[w,w,l,l,l,l,l,l,l,l],
+	[w,w,w,l,l,l,l,l,l,l],
+	[w,w,w,w,l,l,l,l,l,l],
+	[w,w,l,l,l,l,l,l,l,w],
+	[l,l,l,l,l,l,l,w,w,w],
+	[l,l,l,l,l,w,w,w,w,w]	
+	]
+}
