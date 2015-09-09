81ccd0317c1d611eec3f9ea124e7dfff696c2674
Eric Romrell <eric_romrell@byu.edu>
2015-09-09 15:21:12 -0600

    User is now able to move on map with transparency

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,3 @@
-var LEFT_BOUNDARY = 0;
-var TOP_BOUNDARY = 0;
-var RIGHT_BOUNDARY = 9;
-var BOTTOM_BOUNDARY = 9;
 var LEFT = "LEFT";
 var UP = "UP";
 var RIGHT = "RIGHT";
@@ -10,31 +6,28 @@ var DIRECTION_MAP = {}
 
 var currentCell = {};
 currentCell.location = {};
-var allElements = new Array(RIGHT_BOUNDARY + 1);
-for (var i = 0; i <= RIGHT_BOUNDARY; i++) {
-    allElements[i] = new Array(BOTTOM_BOUNDARY + 1);
-}
+var allElements;
+var map;
 
 window.onload=function() {
-    var grid = clickableGrid(10, 10, function(element, row, col) {});
+    map = getMap('map1');
+    var grid = mapGrid(10, 10, map);
     document.getElementById("mainBoard").appendChild(grid);
-    currentCell.element = allElements[0][0];
+    currentCell.element = allElements[5][5];
     currentCell.element.className = 'DOWN';
-    currentCell.location = {row:0,col:0};
+    currentCell.location = {row:5,col:5};
 }
 
-function clickableGrid(rows, cols, callback) {
+function mapGrid(rows, cols, map) {
     var grid = document.createElement('table');
     grid.className = 'grid';
-    for (var row = 0; row < rows; row++){
+    allElements = new Array(map.length)
+    for (var row = 0; row < rows; row++) {
+        allElements[row] = new Array(map[row].length);
         var tr = grid.appendChild(document.createElement('tr'));
         for (var col = 0; col < cols; col++){
             var cell = tr.appendChild(document.createElement('td'));
-            cell.addEventListener('click',(function(element, row, col){
-                return function(){
-                    callback(element, row, col);
-                }
-            })(cell, row, col),false);
+            cell.style.backgroundColor = getColor(map[row][col]);
             allElements[row][col] = cell;
         }
     }
@@ -54,8 +47,19 @@ window.onkeydown = function(e) {
 }
 
 function executeMove(dir) {
-    currentCell.element.className = '';
+    currentCell.element.className = map[currentCell.location.row][currentCell.location.col];
     currentCell.location = move(currentCell.location, dir);
     currentCell.element = allElements[currentCell.location.row][currentCell.location.col];
     currentCell.element.className = dir;
+}
+
+function getColor(tileId) {
+    switch(tileId) {
+        case 'l':
+            return "tan";
+        case 'w':
+            return "blue";
+        default:
+            return "white";
+    }
 }
