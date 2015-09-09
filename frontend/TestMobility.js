c84c3e92cb3a7494a47337383fa43a523b3670dc
Eric Romrell <eric_romrell@byu.edu>
2015-09-09 09:44:55 -0600

    Finished getting person to move around grid. Not animated

--- frontend/TestMobility.js
+++ frontend/TestMobility.js
@@ -1,46 +1,51 @@
+var allCells;
+var currentRow;
+var currentCol;
+
+var currentCell;
+
 window.onload=function() {
-    var lastClicked;
-    var grid = clickableGrid(10,10,function(el,row,col,i){
+    var grid = clickableGrid(10,10,function(el,row,col,i) {
         document.getElementById("col").value = col;
         document.getElementById("row").value = row;
 
-        if (lastClicked) {
-            lastClicked.className='';
+        if (currentCell) {
+            currentCell.className='';
         }
         el.className='clicked';
 
-        lastClicked = el;
+        currentCell = el;
     });
 
-    document.body.appendChild(grid);
+    document.getElementById("mainBoard").appendChild(grid);
 }
 
-function clickableGrid( rows, cols, callback ){
-        var i=0;
-        var grid = document.createElement('table');
-        grid.className = 'grid';
-        for (var r=0;r<rows;r++){
-            var tr = grid.appendChild(document.createElement('tr'));
-            for (var c=0;c<cols;c++){
-                var cell = tr.appendChild(document.createElement('td'));
-                
-                cell.addEventListener('click',(function(el,r,c,i){
-                    return function(){
-                        callback(el,r,c,i);
-                    }
-                })(cell,r,c,i),false);
-            }
+function clickableGrid( rows, cols, callback ) {
+    var i=0;
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;r++){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;c++){
+            var cell = tr.appendChild(document.createElement('td'));
+            
+            cell.addEventListener('click',(function(el,r,c,i){
+                return function(){
+                    callback(el,r,c,i);
+                }
+            })(cell,r,c,i),false);
         }
-        return grid;
     }
+    return grid;
+}
 
 function testCanMove() {
     col = document.getElementById("col").value;
     row = document.getElementById("row").value;
     dir = document.getElementById("dir").value;
     if (canMove({col, row}, dir)) {
-        document.getElementById("result").innerHTML = "That will work!";
+        currentCell.className = 'successCell';
     } else {
-        document.getElementById("result").innerHTML = "You can't do that!";
+        currentCell.className = 'failCell';
     }
 }
---
+++ frontend/index.js
@@ -0,0 +1,61 @@
+var LEFT_BOUNDARY = 0;
+var TOP_BOUNDARY = 0;
+var RIGHT_BOUNDARY = 9;
+var BOTTOM_BOUNDARY = 9;
+var LEFT = "LEFT";
+var UP = "UP";
+var RIGHT = "RIGHT";
+var DOWN = "DOWN";
+var DIRECTION_MAP = {}
+
+var currentCell = {};
+currentCell.location = {};
+var allElements = new Array(RIGHT_BOUNDARY + 1);
+for (var i = 0; i <= RIGHT_BOUNDARY; i++) {
+    allElements[i] = new Array(BOTTOM_BOUNDARY + 1);
+}
+
+window.onload=function() {
+    var grid = clickableGrid(10, 10, function(element, row, col) {});
+    document.getElementById("mainBoard").appendChild(grid);
+    currentCell.element = allElements[0][0];
+    currentCell.element.className = 'clicked';
+    currentCell.location = {row:0,col:0};
+}
+
+function clickableGrid(rows, cols, callback) {
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var row = 0; row < rows; row++){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var col = 0; col < cols; col++){
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.addEventListener('click',(function(element, row, col){
+                return function(){
+                    callback(element, row, col);
+                }
+            })(cell, row, col),false);
+            allElements[row][col] = cell;
+        }
+    }
+    return grid;
+}
+
+window.onkeydown = function(e) {
+    if (e.keyCode === 37 && canMove(currentCell, LEFT)) {
+        executeMove(LEFT);
+    } else if (e.keyCode === 38 && canMove(currentCell, UP)) {
+        executeMove(UP);
+    } else if (e.keyCode === 39 && canMove(currentCell, RIGHT)) {
+        executeMove(RIGHT);
+    } else if (e.keyCode === 40 && canMove(currentCell, DOWN)) {
+        executeMove(DOWN);
+    }
+}
+
+function executeMove(dir) {
+    currentCell.element.className = '';
+    currentCell.location = move(currentCell.location, dir);
+    currentCell.element = allElements[currentCell.location.row][currentCell.location.col];
+    currentCell.element.className = dir;
+}
