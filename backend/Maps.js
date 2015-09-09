caeec93c2026ef563112ebd0e3d37e7912c0c5df
Mark <mzroth@gmail.com>
2015-09-09 16:26:46 -0600

    added quotes to map and maybe added a check to canMove

--- backend/Maps.js
+++ backend/Maps.js
@@ -4,15 +4,15 @@ function getMap(mapName) {
 
 maps = {
 	// w = water l = land
-	map1: [[w,w,w,w,l,l,l,l,l,l],
-	[w,w,w,l,l,l,l,l,l,l],
-	[w,w,l,l,l,l,l,l,l,l],
-	[w,l,l,l,l,l,l,l,l,l],
-	[w,w,l,l,l,l,l,l,l,l],
-	[w,w,w,l,l,l,l,l,l,l],
-	[w,w,w,w,l,l,l,l,l,l],
-	[w,w,l,l,l,l,l,l,l,w],
-	[l,l,l,l,l,l,l,w,w,w],
-	[l,l,l,l,l,w,w,w,w,w]	
+	map1: [["w","w","w","w","l","l","l","l","l","l"],
+	["w","w","w","l","l","l","l","l","l","l"],
+	["w","w","l","l","l","l","l","l","l","l"],
+	["w","l","l","l","l","l","l","l","l","l"],
+	["w","w","l","l","l","l","l","l","l","l"],
+	["w","w","w","l","l","l","l","l","l","l"],
+	["w","w","w","w","l","l","l","l","l","l"],
+	["w","w","l","l","l","l","l","l","l","w"],
+	["l","l","l","l","l","l","l","w","w","w"],
+	["l","l","l","l","l","w","w","w","w","w"]	
 	]
 }
--- backend/Mobility.js
+++ backend/Mobility.js
@@ -3,20 +3,22 @@ var BOTTOM_BONDARY = 9;
 var LEFT_BONDARY = 0;
 var TOP_BONDARY = 0;
 
-function canMove(location, direction) {
-    switch(direction) {
+function canMove(location, direction, map1) {
+    map = getMap("map1")
+	switch(direction) {
         case "LEFT": {
-            return location.col != LEFT_BONDARY;
+			return location.col != LEFT_BONDARY and map1[location.row][location.col-1] !=== "w";
         }
         case "RIGHT": {
-            return location.col != RIGHT_BONDARY;
+            return location.col != RIGHT_BONDARY and map1[location.row][location.col+1] !=== "w";
 		}
 		case "UP": {
-            return location.row != TOP_BONDARY;
+            return location.row != TOP_BONDARY and map1[location.row-1][location.col] !=== "w";
         }
         case "DOWN": {
-            return location.row != BOTTOM_BONDARY;
+            return location.row != BOTTOM_BONDARY and map1[location.row+1][location.col] !=== "w";
         }
+		
     }
 }
 
