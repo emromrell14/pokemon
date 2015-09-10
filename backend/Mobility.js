624b18f003080c6261ad4b11fd913c79d3ae9841
Mark <mzroth@gmail.com>
2015-09-09 20:12:07 -0600

    debugged the turning on the bottom and right sides of the screen due to length error

--- backend/Mobility.js
+++ backend/Mobility.js
@@ -3,22 +3,26 @@ var BOTTOM_BONDARY = 9;
 var LEFT_BONDARY = 0;
 var TOP_BONDARY = 0;
 
+var walkableTiles = ["l","d","g"];
 
+function canWalk (tileId) {
+	return walkableTiles.indexOf(tileId) !== -1;
+}
 
 function canMove(location, direction) {
     mapData = getCurrentMap();
 	switch(direction) {
         case "LEFT": {
-			return location.col != 0 && mapData[location.row][location.col-1] === "l";
+			return location.col != 0 && canWalk(mapData[location.row][location.col-1]);
         }
         case "RIGHT": {
-            return location.col != mapData[0].length && mapData[location.row][location.col+1] === "l";
+            return location.col != mapData[0].length-1 && canWalk(mapData[location.row][location.col+1]);
 		}
 		case "UP": {
-            return location.row != 0 && mapData[location.row-1][location.col] === "l";
+            return location.row != 0 && canWalk(mapData[location.row-1][location.col]);
         }
         case "DOWN": {
-            return location.row != mapData.length && mapData[location.row+1][location.col] === "l";
+            return location.row != mapData.length-1 && canWalk(mapData[location.row+1][location.col]);
         }
 		
     }
