7544befd418e7eb7bb1a2a6de7889fc6f71420d2
Mark <mzroth@gmail.com>
2015-09-10 20:54:49 -0600

    bug fix on mobility

--- backend/Mobility.js
+++ backend/Mobility.js
@@ -3,6 +3,10 @@ var POKEMON_CHANCE = 25;
 var walkableTiles = ["l","d","g"];
 
 var topJumpTiles = ["tc"];
+var rightJumpTiles = ["rc"];
+var leftJumpTiles = ["lc"];
+var bottomJumpTiles = ["bc"]
+
 
 function canWalk (tileId) {
 	return walkableTiles.indexOf(tileId) !== -1;
@@ -14,16 +18,16 @@ function canMove(location, direction) {
     mapData = getCurrentMap();
 	switch(direction) {
         case "LEFT": {
-			return location.col != 0 && canWalk(mapData[location.row][location.col-1]);
+			return location.col != 0 && canWalk(mapData[location.row][location.col-1]) || leftJumpTiles === (mapData[location.row][location.col-1]);
         }
         case "RIGHT": {
-            return location.col != mapData[0].length-1 && canWalk(mapData[location.row][location.col+1]);
+            return location.col != mapData[0].length-1 && canWalk(mapData[location.row][location.col+1]) || rightJumpTiles === (mapData[location.row][location.col-1]);
 		}
 		case "UP": {
-            return location.row != 0 && canWalk(mapData[location.row-1][location.col]);
+            return location.row != 0 && canWalk(mapData[location.row-1][location.col]) || bottomJumpTiles === (mapData[location.row-1][location.col]);
         }
         case "DOWN": {
-            return location.row != mapData.length-1 && canWalk(mapData[location.row+1][location.col]) || topJumpTiles;
+            return location.row != mapData.length-1 && canWalk(mapData[location.row+1][location.col]) || topJumpTiles === (mapData[location.row+1][location.col]);
         }
 		
     }
