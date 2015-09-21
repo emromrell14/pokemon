b954f8221a609f4453581425901b7c3c396f9ca5
Eric Romrell <eric_romrell@byu.edu>
2015-09-21 14:51:01 -0600

    Mocked up some pokemon code

--- backend/Pokemon.js
+++ backend/Pokemon.js
@@ -1,25 +1,23 @@
-// Pokedex
-pokemon = {
-	1 = {
-		ID: "1",
-		Name: "Bulbasaur",
-		Type: ["Grass", "Poison"],
-		Previous: "N/A"
-		Next: "Ivysaur"
+var Pokemon = function(name, nationalId, level, hp, maxHp, exp, maxExp, moves) {
+	this.name = name; 
+	this.nationalId = nationalId;
+	this.level = level;
+	this.hp = hp;
+	this.maxHp = maxHp; 
+	this.exp = exp;
+	this.maxExp = maxExp;
+	this.moves = moves;
+};
+
+Pokemon.createPokemonFromResource = function(pokemon, level) {
+	var moves = [];
+	for (var i = 0; i < pokemon.moves.length; i++) {
+		if (pokemon.moves[i].learn_type === "level up" && pokemon.moves[i].level < level) {
+			moves.push(pokemon.moves[i]);
+			if (moves.length > 4) {
+				moves.splice(0, 1);
+			}			
+		}
 	}
-	2 = {
-		ID: "2",
-		Name: "Ivysaur",
-		Type: ["Grass", "Poison"],
-		Previous: "Bulbasaur"
-		Next: "Venusaur"
-	}
-	3 = {
-		ID: "3",
-		Name: "Venusaur",
-		Type: ["Grass", "Poison"],
-		Previous: "Venusaur"
-		Next: "N/A"
-	}
-	
-}
+	return new Pokemon(pokemon.name, pokemon.national_id, level, 40, 40, 0, 200, moves);
+};
