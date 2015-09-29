ce21dcace8097de1d1ae9544bbf50302a893d65c
Eric Romrell <eric_romrell@byu.edu>
2015-09-29 10:20:13 -0600

    Fixed issue with continuing to move after leaving map

--- backend/Mobility.js
+++ backend/Mobility.js
@@ -1,6 +1,6 @@
-var POKEMON_CHANCE = 20;
+var POKEMON_CHANCE = 200;
 
-var walkableTiles = ["groundGreen","groundBrown","groundWhite","groundYellow","door","grass"];
+var walkableTiles = ["groundGreen", "groundBrown", "groundWhite", "groundYellow", "door", "grass"];
 
 var leftJumpTile = "cliffLeft";
 var rightJumpTile = "cliffRight";
@@ -13,87 +13,83 @@ Mobility.canWalk = function(tileId) {
 };
 
 Mobility.canMove = function(location, direction) {
-    var mapData = Maps.getCurrentMap();
+	var mapData = Maps.getCurrentMap();
 	var nextLoc;
-	switch(direction) {
-        case "LEFT": {
-			if (location.col == 0){
-				return true;
-			}
+	switch (direction) {
+		case "LEFT":
+			if (location.col == 0) return true;
+			if (location.col < 0) return false;
+
 			nextLoc = mapData[location.row][location.col - 1];
-			if (nextLoc === leftJumpTile){
+			if (nextLoc === leftJumpTile) {
 				return true;
 			}
 			break;
-        }
-        case "RIGHT": {
-            if (location.col == mapData[0].length - 1){
-				return true;
-			}
+		case "RIGHT":
+			if (location.col == mapData[0].length - 1) return true;
+			if (location.col > mapData[0].length - 1) return false;
+
 			nextLoc = mapData[location.row][location.col + 1];
-			if (nextLoc === rightJumpTile){
+			if (nextLoc === rightJumpTile) {
 				return true;
 			}
 			break;
-		}
-		case "UP": {
-            if (location.row == 0){
-				return true;
-			}
+		case "UP":
+			if (location.row == 0) return true;
+			if (location.row < 0) return false;
+
 			nextLoc = mapData[location.row - 1][location.col];
 			break;
-        }
-        case "DOWN": {
-            if (location.row == mapData.length - 1){
-				return true;
-			}
+		case "DOWN":
+			if (location.row == mapData.length - 1) return true;
+			if (location.row > mapData.length - 1) return false;
+
 			nextLoc = mapData[location.row + 1][location.col];
-			if (nextLoc === downJumpTile){
+			if (nextLoc === downJumpTile) {
 				return true;
 			}
 			break;
-        }
-    }
+	}
 	return Mobility.canWalk(nextLoc);
 };
 
 Mobility.move = function(location, direction) {
-    //first check if the user can move 
-    if(!Mobility.canMove(location, direction)) {
-        return location;
-    }
+	//first check if the user can move
+	if (!Mobility.canMove(location, direction)) {
+		return location;
+	}
 	var mapData = Maps.getCurrentMap();
 	var oldLocation = {row: location.row, col: location.col};
-	switch(direction) {
+	switch (direction) {
 		case "LEFT":
 			location.col -= 1;
 			break;
-		case "RIGHT": 
+		case "RIGHT":
 			location.col += 1;
 			break;
-		case "UP": 
+		case "UP":
 			location.row -= 1;
 			break;
-		case "DOWN": 
+		case "DOWN":
 			location.row += 1;
 			break;
 	}
-	if (location.col == -1 || location.row == -1 || 
-			location.col == mapData[0].length || location.row == mapData.length) {
+	if (location.col == -1 || location.row == -1 ||
+		location.col == mapData[0].length || location.row == mapData.length) {
 		return Maps.getLinkingLocation(Maps.getCurrentMapName(), oldLocation);
 	}
 	var nextTile = mapData[location.row][location.col];
-	
+
 	Mobility.queryForPokemon(nextTile);
 
 	return {mapName: currentMapName, location: location};
-	
-	
+
+
 };
 
 Mobility.queryForPokemon = function(tileId) {
 	if (tileId == "grass") {
-		var rand = Math.floor(Math.random()*POKEMON_CHANCE);
+		var rand = Math.floor(Math.random() * POKEMON_CHANCE);
 		if (rand == 0) {
 			setInFight(true);
 			foundPokemon(Maps.getRandomPokemonForCurrentMap());
